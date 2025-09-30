93eaf20334b4740e1d7848e3816bd175df3a2827
bulbul1945 <ervashohibulkahfi@gmail.com>
2025-09-30 16:49:13 +0700

    controller untuk test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Calon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    function x()
+    {
+        $data = Calon::with('misi')->find(1);
+
+        foreach ($data->misi as $misi) {
+            echo $misi->misi . '<br>';
+        }
+    }
 }
